c69f4482dfe9b3257439c6002ca5d4b5c5df426a
Johan Durish <jdurish2@gmail.com>
2025-02-21 13:25:39 -0500

    Move library functions into library class

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-const myLibrary = [];
-
 class Book {
    constructor(title, author, pages, read) {
       this.title = title;
@@ -8,31 +6,38 @@ class Book {
       this.pages = pages;
       this.read = read;
    }
-
+   
    info() {
       return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"}`;
    }
 }
 
-const addBookToLibrary = (book, library) => {
-   library.push(book);
-}
-
-const renderLibrary = (library, libraryElement) => {
-   while (libraryElement.firstChild) {
-      libraryElement.removeChild(libraryElement.lastChild);
+class Library {
+   constructor() {
+      this.books = [];
    }
-   
-   library.forEach(book => {
-      let bookElement = document.createElement("li");
-      bookElement.innerText = book.info();
-      libraryElement.appendChild(bookElement);
-   });
+
+   addBook(book) {
+      this.books.push(book);
+   }
+
+   render(libraryElement) {
+      while (libraryElement.firstChild) {
+         libraryElement.removeChild(libraryElement.lastChild);
+      }
+      
+      this.books.forEach(book => {
+         let bookElement = document.createElement("li");
+         bookElement.innerText = book.info();
+         libraryElement.appendChild(bookElement);
+      });
+   }
 }
 
+const myLibrary = new Library();
 const libraryElement = document.querySelector(".library");
 
 for (let i = 1; i <= 5; i++) {
-   addBookToLibrary(new Book(`Book ${i}`, `Author ${i}`, i * 100, false), myLibrary);
-   renderLibrary(myLibrary, libraryElement);
+   myLibrary.addBook(new Book(`Book ${i}`, `Author ${i}`, i * 100, false));
+   myLibrary.render(libraryElement);
 }
